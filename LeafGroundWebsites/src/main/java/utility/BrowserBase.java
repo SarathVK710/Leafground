90b2a32e82b8394dad98e8e2dca9da5eaef0caa8
sarath <sarathvk710@gmail.com>
2026-03-30 15:08:11 +0530

    browser

--- LeafGroundWebsites/src/main/java/utility/BrowserBase.java
+++ LeafGroundWebsites/src/main/java/utility/BrowserBase.java
@@ -1,8 +1,10 @@
 package utility;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -26,20 +28,18 @@ public class BrowserBase {
             FileInputStream fileInputStream = new FileInputStream(file);
             properties.load(fileInputStream);
 
-//        if (properties.getProperty("browser").equalsIgnoreCase("chrome")){
-//             driver = new ChromeDriver();
-//        }else if (properties.getProperty("browser").equalsIgnoreCase("edge")) {
-//             driver = new EdgeDriver();
-//        }else if (properties.getProperty("browser").equalsIgnoreCase("safari")){
-//             driver = new SafariDriver();
-//        }else {
-//            throw new InvalidArgumentException("Enter your correct browser name");
-//        }
-
-
             switch (properties.getProperty("browser")) {
                 case "chrome":
-                    driver = new ChromeDriver();
+                    //driver = new ChromeDriver();
+                    WebDriverManager.chromedriver().setup();
+
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--headless=new");
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                    options.addArguments("--window-size=1920,1080");
+
+                    driver = new ChromeDriver(options);
                     break;
                 case "edge":
                     driver = new EdgeDriver();
@@ -55,9 +55,22 @@ public class BrowserBase {
             }
 
             driver.get(properties.getProperty("testenvironment"));
-            driver.manage().window().maximize();
+  //          driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         }
        return driver;
     }
 }
+
+
+
+
+//        if (properties.getProperty("browser").equalsIgnoreCase("chrome")){
+//             driver = new ChromeDriver();
+//        }else if (properties.getProperty("browser").equalsIgnoreCase("edge")) {
+//             driver = new EdgeDriver();
+//        }else if (properties.getProperty("browser").equalsIgnoreCase("safari")){
+//             driver = new SafariDriver();
+//        }else {
+//            throw new InvalidArgumentException("Enter your correct browser name");
+//        }
